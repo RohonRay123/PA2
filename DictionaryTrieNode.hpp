daae345e011329d049f5db9a8fff8074b9802f8d
RohonRay123 <50560353+RohonRay123@users.noreply.github.com>
2021-07-14 10:04:32 -0700

    Update DictionaryTrieNode.hpp

--- DictionaryTrieNode.hpp
+++ DictionaryTrieNode.hpp
@@ -1,1 +1,45 @@
+/**
+ * DictionaryTrieNode.hpp
+ *
+ * Author: Rohon Ray
+ */
+#ifndef DICTIONARY_TRIE_NODE_HPP
+#define DICTIONARY_TRIE_NODE_HPP
+
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+/**
+ * The class for a dictionary ADT, implemented as either
+ * a mulit-way trie or a ternary search tree.
+ */
+
+class DictionaryTrieNode {
+  public: 
+     int wordFrequency;
+     bool isWord;
+     char character;
+     DictionaryTrieNode* left;
+     DictionaryTrieNode* right; 
+     DictionaryTrieNode* middle; 
+     
+   DictionaryTrieNode(char character, DictionaryTrieNode* left,
+   DictionaryTrieNode* right,DictionaryTrieNode* middle,
+   int wordFrequency,bool isWord)
+   {
+       this->left=left;
+       this->right=right;
+       this->middle=middle; 
+       this->wordFrequency=wordFrequency;
+       this->isWord=isWord;
+       this->character=character;
+       
+   }
+
+};
+
+#endif  // DICTIONARY_TRIE_NODE_HPP
 
